df0c81ad92a0a9151042c6a8bf6a43f9cc1d4a6f
kkosuge <kkosuge@about.me>
2012-01-09 17:39:18 +0900

    Tweetbotからアップロードできるようにした

--- app.rb
+++ app.rb
@@ -17,6 +17,20 @@ def randstr(i=3)
   Array.new(i){(('a'..'z').to_a+('A'..'Z').to_a+('0'..'9').to_a)[rand(62)]}.join
 end
 
+def set_image(imagedata)
+  file_name = "#{randstr}.png"
+  file_path = "#{options.image_dir}/#{file_name}"
+
+  File.open("#{file_path}","w").print(imagedata)
+  image = Magick::Image.read("#{file_path}").first
+  image.resize_to_fit!(200,200)
+  image.write("#{options.image_dir}/thumb/#{file_name}")
+ 
+  Image.create(file_name: "#{file_name}")
+
+  "#{options.image_url}/#{file_name}"
+end
+
 configure do
   set :gyazo_id , 'sample'
   set :image_dir, 'public'
@@ -32,15 +46,11 @@ post '/upload/gyazo' do
   raise unless params['id'] == options.gyazo_id
 
   imagedata = params['imagedata'][:tempfile].read
-  file_name = "#{randstr}.png"
-  file_path = "#{options.image_dir}/#{file_name}"
+  set_image(imagedata)
+end
 
-  File.open("#{file_path}","w").print(imagedata)
-  image = Magick::Image.read("#{file_path}").first
-  image.resize_to_fit!(200,200)
-  image.write("#{options.image_dir}/thumb/#{file_name}")
- 
-  Image.create(file_name: "#{file_name}")
-
-  "#{options.image_url}/#{file_name}"
+post '/tweetbot-upload' do
+  imagedata = params['media'][:tempfile].read
+  content_type :json
+  { :url => set_image(imagedata) }.to_json
 end
