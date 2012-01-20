7c239e7ab00618d5b1f13bf021dbab75bbddd6f5
kkosuge <kkosuge@about.me>
2012-01-21 02:14:29 +0900

    RMagick使うのやめた

--- app.rb
+++ app.rb
@@ -4,8 +4,6 @@ require 'sinatra'
 require 'haml'
 require 'json'
 require 'mongoid'
-gem "rmagick", :require => "RMagick"
-require 'RMagick'
 $:.unshift(File.dirname(__FILE__))
 require 'models/image'
 
@@ -13,29 +11,33 @@ Mongoid.configure do |config|
   config.master = Mongo::Connection.new.db('pic')
 end
 
-def randstr(i=3)
-  Array.new(i){(('a'..'z').to_a+('A'..'Z').to_a+('0'..'9').to_a)[rand(62)]}.join
-end
-
-def set_image(imagedata)
-  file_name = "#{randstr}.png"
-  file_path = "#{options.image_dir}/#{file_name}"
-
-  File.open("#{file_path}","w").print(imagedata)
-  image = Magick::Image.read("#{file_path}").first
-  image.resize_to_fit!(200,200)
-  image.write("#{options.image_dir}/thumb/#{file_name}")
- 
-  Image.create(file_name: "#{file_name}")
-
-  "#{options.image_url}/#{file_name}"
-end
-
 configure do
   set :gyazo_id , 'sample'
   set :image_dir, 'public'
   set :image_url, 'http://pic.kksg.net'
 end
+
+def randstr(i=3)
+  Array.new(i){(('a'..'z').to_a+('A'..'Z').to_a+('0'..'9').to_a)[rand(62)]}.join
+end
+
+def gen_file_name
+  name = randstr
+  while Image.exists?(conditions: { file_name: name })
+    name = randstr
+  end
+  name
+end
+
+def set_image(imagedata)
+  file_name = "#{gen_file_name}.png"
+  file_path = "#{options.image_dir}/#{file_name}"
+  File.open("#{file_path}","w").print(imagedata)
+  Image.create(file_name: "#{file_name}")
+
+  "#{options.image_url}/#{file_name}"
+end
+
 
 get '/' do
   @images = Image.all.desc(:updated_at)
